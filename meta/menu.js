018197e8b15013dbbd8792ca0710b520d510a68c
Pierre Fenoll <pierrefenoll@gmail.com>
2014-07-21 15:23:24 +0200

    Move menu out into a single static place.

---
+++ meta/menu.js
@@ -0,0 +1,38 @@
+var menu = " \
+    <div id=\"menu\" data-markdown> \
+      # [ELSA FARBOS](index.htm) \
+      <br/> \
+      ### 2013-2014 \
+      * [Eine Mauer wie eine Frau](2013/eine-mauer-wie-eine-frau/) \
+      * [fémur et humérus](2013/femur-et-humerus/) \
+      ### 2012 \
+      * [apparition](clients/apparition/) \
+      * [balai bois](clients/balai-bois/) \
+      * [balai gaz](clients/balai-gaz/) \
+      * [crânes brûlés](clients/cranes-brules/) \
+      * [en bas de l'échelle](clients/en-bas-de-lechelle/) \
+      ### 2011 \
+      * [Ninfas Méduas](clients/ninfas-meduas/) \
+      * [épicentre](clients/epicentre/) \
+      * [périmètre(s)](clients/perimetres/) \
+      * [crâne image](clients/crane-image/) \
+      * [crânes béton](clients/cranes-beton/) \
+      ### 2010 \
+      * [images de fer](clients/images-de-fer/) \
+      * [nature piquée](clients/nature-pique/) \
+      * [dispersion](clients/projet-de-fer/) \
+      ### Texte \
+      * [J'écris](project/jecris/) \
+      * [Percer, crever, éclater](project/percer-crever-eclater/) \
+      ### image d'expo \
+      * [MONDEMON](image-dexpo/mondemon/) \
+      * [Nuit des musées](clients/nuit-des-musees/) \
+      * [Après Nicolas](clients/apres-nicolas/) \
+      * [Esads, lieux d'expositions](image-dexpo/dnsep/) \
+      ### Info \
+      * [contact](contact/) \
+      * [home](index.htm) \
+      * [biographie](clients/sony/) \
+      * [liens](liens/) \
+    </div> \
+";
