3d128692bc0c60930390ab535bea99353799e6dc
moltmoltmolt <mathilde.fenoll@gmail.com>
2014-07-21 15:29:39 +0200

    Update menu.js
    Jai change le nom

--- meta/menu.js
+++ meta/menu.js
@@ -1,6 +1,6 @@
 var menu = " \
     <div id=\"menu\" data-markdown> \
-      # [ELSA FARBOS](index.htm) \
+      # [MATHILDE FENOLL](index.htm) \
       <br/> \
       ### 2013-2014 \
       * [Eine Mauer wie eine Frau](2013/eine-mauer-wie-eine-frau/) \
