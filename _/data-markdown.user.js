e4555a1c2bc9438e3c3def81be92004092016474
Pierre Fenoll <pierrefenoll@gmail.com>
2014-07-21 12:19:53 +0200

    Moving to markdown. Refonte of some sources.

--- _/data-markdown.user.js
+++ _/data-markdown.user.js
@@ -4,34 +4,31 @@
 // @link http://git.io/data-markdown
 // @match *
 // ==/UserScript==
- 
- 
+
+
 // If you're not using this as a userscript just delete from this line up. It's cool, homey.
- 
+
 (function boom(){
-  
-  if (!window.Showdown){
-    var scr = document.createElement('script');
-    scr.onload = boom;
-    scr.src = '_/showdown.js';
-//  scr.src = 'https://raw.github.com/github/github-flavored-markdown/gh-pages/scripts/showdown.js';
-    document.body.appendChild(scr);
-    return;
-  }
- 
-  [].forEach.call( document.querySelectorAll('[data-markdown]'), function  fn(elem){
-      
-    // strip leading whitespace so it isn't evaluated as code
-    var text      = elem.innerHTML.replace(/\n\s*\n/g,'\n'),
+
+    if (!window.Showdown){
+        var scr = document.createElement('script');
+        scr.onload = boom;
+        scr.src = '_/showdown.js';
+        document.body.appendChild(scr);
+        return;
+    }
+
+    [].forEach.call( document.querySelectorAll('[data-markdown]'), function (elem) {
+        // strip leading whitespace so it isn't evaluated as code
+        var text      = elem.innerHTML.replace(/\n\s*\n/g,'\n');
         // set indentation level so your markdown can be indented within your HTML
-        leadingws = text.match(/^\n?(\s*)/)[1].length,
-        regex     = new RegExp('\\n?\\s{' + leadingws + '}','g'),
-        md        = text.replace(regex,'\n'),
-        html      = (new Showdown.converter()).makeHtml(md);
- 
-    // here, have sum HTML
-    elem.innerHTML = html;
- 
-  });
- 
+        var leadingws = text.match(/^\n?(\s*)/)[1].length;
+        var regex     = new RegExp('\\n?\\s{' + leadingws + '}','g');
+        var md        = text.replace(regex,'\n');
+        var html      = (new Showdown.converter()).makeHtml(md);
+
+        // here, have sum HTML
+        elem.innerHTML = html;
+    });
+
 }());
