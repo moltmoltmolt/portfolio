2d63f9ca1e4ffb734a2f965ded69d354176809ee
moltmoltmolt <mathilde.fenoll@gmail.com>
2014-07-21 15:31:18 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -5,31 +5,31 @@ var menu = " \
       ##### 2013-2014 \
       * [Eine Mauer wie eine Frau](2013/eine-mauer-wie-eine-frau/) \
       * [fémur et humérus](2013/femur-et-humerus/) \
-      ### 2012 \
+      ##### 2012 \
       * [apparition](clients/apparition/) \
       * [balai bois](clients/balai-bois/) \
       * [balai gaz](clients/balai-gaz/) \
       * [crânes brûlés](clients/cranes-brules/) \
       * [en bas de l'échelle](clients/en-bas-de-lechelle/) \
-      ### 2011 \
+      ##### 2011 \
       * [Ninfas Méduas](clients/ninfas-meduas/) \
       * [épicentre](clients/epicentre/) \
       * [périmètre(s)](clients/perimetres/) \
       * [crâne image](clients/crane-image/) \
       * [crânes béton](clients/cranes-beton/) \
-      ### 2010 \
+      ##### 2010 \
       * [images de fer](clients/images-de-fer/) \
       * [nature piquée](clients/nature-pique/) \
       * [dispersion](clients/projet-de-fer/) \
-      ### Texte \
+      ##### Texte \
       * [J'écris](project/jecris/) \
       * [Percer, crever, éclater](project/percer-crever-eclater/) \
-      ### image d'expo \
+      ##### image d'expo \
       * [MONDEMON](image-dexpo/mondemon/) \
       * [Nuit des musées](clients/nuit-des-musees/) \
       * [Après Nicolas](clients/apres-nicolas/) \
       * [Esads, lieux d'expositions](image-dexpo/dnsep/) \
-      ### Info \
+      ##### Info \
       * [contact](contact/) \
       * [home](index.htm) \
       * [biographie](clients/sony/) \
