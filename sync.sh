e4b6bfa8e517c0fe9e8873df4778399ca6a1ddd4
Pierre Fenoll <pierrefenoll@gmail.com>
2014-07-23 22:58:19 +0200

    le sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd /Users/boubou/website/portfolio.git
-git pull origin gh-pages
-git add -A .
-git commit -am 'changes on Boubou'
-git push origin gh-pages
+P () {
+    echo -e "\033[01;34m$CNT :: \033[00m\033[01;01m $1\033[00m"
+    ((CNT--))
+}
 
+cd "$(dirname "$0")" \
+    && P 'git pull origin gh-pages --no-commit' \
+    && git pull origin gh-pages --no-commit \
+    && P 'git add -A .' \
+    && git add -A . \
+    && git commit -am 'changes on Boubou' \
+    && P 'git push origin gh-pages' \
+    && git push origin gh-pages
