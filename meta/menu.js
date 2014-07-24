25e89cccc5f3e60fd3a85c1c342144c6b581c79a
moltmoltmolt <mathilde.fenoll@gmail.com>
2014-07-24 16:48:01 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -2,30 +2,21 @@ var menu = " \
     <div id=\"menu\" data-markdown> \
       # [MATHILDE FENOLL](/) \
       <br/> \
-      ##### 2013 \
+      ##### Works \
       * [Malacologie](/malacologie.htm) \
       * [Néons](/neons.htm) \
-      * [Malacologie](/malacologie.htm) \
-      ##### 2012 \
+      * [Domestic Disturbance](/poussiere.htm) \
+      * [Diplopie](/diplopie.htm) \
+      * [La Terre Est Faite De Ciel](/papier_peint.htm) \
       * [Ziggurats](/ziggurats.htm/) \
-      ##### 2011 \
       * [Coloriages](/plage.htm) \
       * [Les Hôtes](/les_hotes.htm/) \
-      * [périmètre(s)](/clients/perimetres/) \
-      * [crâne image](/clients/crane-image/) \
-      * [crânes béton](/clients/cranes-beton/) \
-      ##### 2010 \
-      * [images de fer](/clients/images-de-fer/) \
-      * [nature piquée](/clients/nature-pique/) \
-      * [dispersion](/clients/projet-de-fer/) \
-      ##### Texte \
+      ##### Drawings \
+      * [Bestiaire](/bestiaire.htm/) \
+      ##### Texts \
       * [J'écris](/project/jecris/) \
-      * [Percer, crever, éclater](/project/percer-crever-eclater/) \
-      ##### éditions \
+      ##### Prints \
       * [Vrac](/vrac.htm/) \
-      * [Nuit des musées](/clients/nuit-des-musees/) \
-      * [Après Nicolas](/clients/apres-nicolas/) \
-      * [Esads, lieux d'expositions](/image-dexpo/dnsep/) \
       ##### Info \
       * [contact](/contact.htm) \
       * [home](/) \
