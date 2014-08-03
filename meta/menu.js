5197e8002360ce86387b291189c75aad85de6b9a
moltmoltmolt <mathilde.fenoll@gmail.com>
2014-08-03 22:39:02 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -13,14 +13,12 @@ var menu = " \
       * [Domestic Disturbance](/domestic_disturbance.htm) \
       * [Diplopie](/diplopie.htm) \
       * [Papier Peint Atmosphérique](/papier_peint.htm) \
-      * [Ziggurats](/ziggurats.htm) \
       * [Coloriages](/coloriages.htm) \
       * [Sans Titre 1](/sans_titre1.htm) \
       * [Les Hôtes](/les_hotes.htm) \
       * [Enfiler Des Perles](/enfiler_des_perles.htm) \
       * [Notes pour sculptures](/encre.htm) \
       * [Études Totémiques](/etudes_totemiques.htm) \
-      * [Collages](/collages.htm) \
       * [Vrac](/vrac.htm) \
       #### INFO \
       * [contact](/contact.htm) \
