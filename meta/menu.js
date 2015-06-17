3921b3d2900daf96461486c4202496a29df8f3c2
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2015-06-17 03:49:17 -0700

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -9,8 +9,6 @@ var menu = " \
       <br/> \
       [CV](/cv.htm) \
       <br/> \
-      [à propos](/texte/texte.htm) \
-      <br/> \
       #### TRAVAUX \
       * [Entre les mondes](/entre_les_mondes.htm) \
       * [User la Mine_1](/user_la_mine_1.htm) \
