fa5c3dd6fbc43eae0f9ccf674566a873f35107b5
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2020-01-20 03:57:57 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -1,6 +1,6 @@
 var menu = " \
     <div id=\"menu\" data-markdown> \
-      [MATHILDE FENOLL](/index.htm) \
+      [M A T H I L D E F E N O L L ](/index.htm) \
       <br/> \
       <br/> \
       [Velella Velella](/velella.htm) \
