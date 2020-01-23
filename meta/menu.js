a9d59eb27aee02f2c9b16f8bbceb3228b3a219f3
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2020-01-23 12:13:38 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -5,53 +5,6 @@ var menu = " \
       <br/> \
       [Velella Velella](/velella.htm) \
       <br/> \
-      [Venus](/butterfly.htm) \
-      <br/> \
-      [Parade](/parade.htm) \
-      <br/> \
-      [Das Netz](/netz.htm) \
-      <br/> \
-      [Le Ring](/lering.htm) \
-      <br/> \
-      [Parabole](/kiki.htm) \
-      <br/> \
-      [Ka](/nez.htm) \
-      <br/> \
-      [Les Crocs](/crocs.htm) \
-      <br/> \
-      [Paradigme](/user-1.htm) \
-      <br/> \
-      [Les Invisibles](/lino.htm) \
-      <br/> \
-      [Reef](/rock.htm) \
-      <br/> \
-      [Vague](/vague.htm) \
-      <br/> \
-      [Travel of the intangibles](/element.htm) \
-      <br/> \
-      [River Mind](/user-2.htm) \
-      <br/> \
-      [Entre les mondes](/entre_les_mondes.htm) \
-      <br/> \
-      [Dentifrication](/dentifrication.htm) \
-      <br/> \
-      [Anatomie](/anat.htm) \
-      <br/> \
-      [Ziggurat](/ziggurat.htm) \
-      <br/> \
-      [Malacologie](/malacologie.htm) \
-      <br/> \
-      [Perspective atmosphérique](/papier_peint.htm) \
-      <br/> \
-      [Neons](/neons.htm) \
-      <br/> \
-      [Strates](/strates.htm) \
-      <br/> \
-      [Les Hôtes](/les_hotes.htm) \
-      <br/> \
-      [RGB](/rgb.htm) \
-      <br/> \
-      [Notes](/encre.htm) \
       <br/> \
       <br/> \
       <br/> \
