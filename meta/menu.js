a92fbc21fcb62865f21d14145496263ce94d5285
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2020-02-29 22:15:27 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -3,6 +3,14 @@ var menu = " \
     [mathilde.fenoll@gmail.com](/index.htm)
     <br/> \ 
     <br/> \
+      <br/>
+      <br/>
+      <br/>
+      [/about](/about.htm)
+      [Silence Forêt](http://silenceforet.com/)
+      [Au Secours](http://ausecours.top/)
+      [Zine Q](/Cacticule.htm) 
+      <br/>
     [|||](/velella.htm) \
       <br/> \
       <br/> \
