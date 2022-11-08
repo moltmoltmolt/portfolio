d1a9018f14110e96d137c27fa0a473035ff7966c
Mathilde Fenoll <mathilde.fenoll@gmail.com>
2022-11-08 23:01:59 +0100

    changes on Boubou

--- meta/menu.js
+++ meta/menu.js
@@ -1,16 +1,16 @@
 var menu = " \
-    <div id=\"menu\" data-markdown> /> 
+    <div id=\"menu\" data-markdown> /> \
     [mathilde.fenoll@gmail.com](/index.htm) \
-    <br/> \ 
     <br/> \
-      <br/>
-      <br/>
-      <br/>
+    <br/> \
+      <br/> \
+      <br/> \
+      <br/> \
       [/about](/about.htm) \
       [Silence Forêt](http://silenceforet.com/) \
       [Au Secours](http://ausecours.top/) \
-      [Zine Q](/Cacticule.htm) \
-      <br/>
+      [Zinewef wef wef wef wef Q](/Cacticule.htm) \
+      <br/> \
     [|||](/velella.htm) \
       <br/> \
       <br/> \
